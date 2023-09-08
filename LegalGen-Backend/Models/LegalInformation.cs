b5430be20417a5e52aaa36639398474c48dfbff6
hiren979 <hirenchavda141@gmail.com>
2023-09-08 19:17:10 +0530

    Update Research Book

--- LegalGen-Backend/Models/LegalInformation.cs
+++ LegalGen-Backend/Models/LegalInformation.cs
@@ -1,20 +1,21 @@
 ﻿using System.ComponentModel.DataAnnotations.Schema;
 using System.ComponentModel.DataAnnotations;
-
 namespace LegalGen_Backend.Models
 {
     public class LegalInformation
     {
-
-        public int Id { get; set; }
-        public string Type { get; set; }
-        public string Title { get; set; }
-        public string Description { get; set; }
-        public string Document { get; set; }
-        public DateTime DateAdded { get; set; }
-
-        public int ResearchBookId { get; set; }
-        //public ResearchBook ResearchBook { get; set; }
-
+        public int? Id { get; set; }
+        public string? CaseType { get; set; }
+        public string? Section { get; set; }
+        public string? CaseNumber { get; set; }
+        public string? Citation { get; set; }
+        public string? Act { get; set; }
+        public string? Petitioner { get; set; }
+        public string? Respondent { get; set; }
+        public string? Description { get; set; }
+        public string? Document { get; set; }
+        public DateTime? DateAdded { get; set; }
+        public int? ResearchBookId { get; set; }
+        public ResearchBook? ResearchBook { get; set; }
     }
 }
